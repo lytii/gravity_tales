a95007e5efd3c0d495860ba332e5642a456541d4
Long <noirlune@gmail.com>
2017-08-08 23:34:52 -0700

    Add test for 2 bookUrls
    add prev/next chapter links to 0,1 of paragraphs
    add parsing for deathmarch
    cleanup removeBlanks to method

--- app/src/main/java/read/gravitytales/util/ChapterParser.java
+++ app/src/main/java/read/gravitytales/util/ChapterParser.java
@@ -2,6 +2,7 @@ package read.gravitytales.util;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
@@ -18,25 +19,13 @@ public final class ChapterParser {
       if (chapterContent.size() == 0) // backup parsing
          chapterContent = toParse.select("div [itemprop='articleBody']");
       List<Node> all = chapterContent.get(0).childNodes();
-      ArrayList<String> paragraphs = new ArrayList<>();
-      int i = 0;
-      boolean first = false; // to remove multiple new lines in a row
-      for (Node node : all) {
-         String string = node.toString().replaceAll("<p[^>]+>|</p>|<p>", "");
-         if (!string.equals(" ")
-               && !string.equals("<hr>")
-               && !string.equals("<br>")
-               && !string.equals("&nbsp;")) {
-            if (string.length() == 0 && first) {
-               // second new line, don't add it
-               first = false;
-            } else {
-               // first new line usually has meaning
-               first = string.length() == 0;
-               paragraphs.add(string.replaceAll("&nbsp;", ""));
-            }
-         }
-      }
+
+      // add prev/next links to 0,1
+      Elements links = chapterContent.select("[href]");
+      all.add(0, new Element(links.get(1).attr("href"))); // Next Chapter Link
+      all.add(0, new Element(links.get(0).attr("href"))); // Previous Chapter Link
+
+      ArrayList<String> paragraphs = removeBlanks(all);
       if (paragraphs.get(0).contains("Next Chapter") || paragraphs.get(0).contains("Previous Chapter")) {
          String title = paragraphs.get(0);
          Elements spanSelect = Jsoup.parse(title).select("span");
@@ -48,5 +37,38 @@ public final class ChapterParser {
       return paragraphs;
    }
 
+   public static ArrayList<String> dmParse(ResponseBody responseBody) throws IOException {
+      Document toParse = Jsoup.parse(responseBody.string());
+      Elements chapterContent = toParse.select("div [class='post-body entry-content']");
+      List<Node> paragraphNodes = chapterContent.get(0).childNodes();
 
+      // add prev/next links to 0,1
+      Elements links = chapterContent.select("[href]");
+      paragraphNodes.add(0, new Element(links.get(1).attr("href"))); // Next Chapter Link
+      paragraphNodes.add(0, new Element(links.get(0).attr("href"))); // Previous Chapter Link
+
+      return removeBlanks(paragraphNodes);
+   }
+
+   public static ArrayList<String> removeBlanks(List<Node> nodes) {
+      ArrayList<String> paragraphs = new ArrayList<>();
+      boolean first = false; // to remove multiple new lines in a row
+      for (Node node : nodes) {
+         String string = node.toString().replaceAll("<p[^>]+>|</p>|<p>", "");
+         if (!string.equals(" ")
+                 && !string.equals("<hr>")
+                 && !string.equals("<br>")
+                 && !string.equals("&nbsp;")) {
+            if (string.length() == 0 && first) {
+               // second new line, don't add it
+               first = false;
+            } else {
+               // first new line usually has meaning
+               first = string.length() == 0;
+               paragraphs.add(string.replaceAll("&nbsp;", ""));
+            }
+         }
+      }
+      return paragraphs;
+   }
 }
